a611d6eb0d77ab3dbbb98e3942627204cb2b1ca7
Mahdi Guliyev <quliyev.mehdi.1997@gmail.com>
2023-04-30 15:17:08 +0400

    added prev and next functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,58 @@
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import people from "./data";
+import { useState } from "react";
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, image, job, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+
+    if (number < 0) {
+      return people.length - 1;
+    }
+
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+      </article>
+    </main>
+  );
 };
 export default App;
